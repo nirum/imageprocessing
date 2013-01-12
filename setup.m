dffcdb2c7c85e4646f9761cdea64fd49e16c6366
Niru Maheswaranathan <nirum@stanford.edu>
2013-01-11 16:02:44 -0800

    adding setup.m

---
+++ setup.m
@@ -0,0 +1,15 @@
+%Setup for image processing toolbox
+%
+% Set up path locations and other information for use
+% with the imageprocessing toolbox.
+%
+% VERSION 1.0, Fri Jan 11 15:45:17 2013         Initial version
+%
+% AUTHOR: Niru Maheswaranathan
+%         nirum@stanford.edu
+
+% Add path to helper functions
+addpath('helper_functions/');
+
+% Location of van Hateren .iml image database
+vanhateren = '~/Documents/Data/images/';
